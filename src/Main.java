a85d81673e3875e49ed64cc92a4e82e86682b5f7
Carlos Arteaga <dakaricarlos@gmail.com>
2022-08-17 18:36:34 -0300

    primer commit aprendiendo git

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main  (String args []){
+
+        int patente = 1233;
+        String modelo = "Volkswagen";
+        System.out.println("La patente es "+patente );
+        System.out.printf("Y elmodelo es "+modelo);
+
+
+
+
+
+    }
+}
