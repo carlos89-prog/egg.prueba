7c58b0a8d152265af46f1b9b9c7e76ec488f85f3
Carlos Arteaga <dakaricarlos@gmail.com>
2022-08-17 18:59:32 -0300

    mi primer commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main  (String args []){
 
         int patente = 1233;
+        double doble = 2.42;
         String modelo = "Volkswagen";
         System.out.println("La patente es "+patente );
         System.out.printf("Y elmodelo es "+modelo);
